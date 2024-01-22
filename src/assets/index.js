587eb18f5c0ff3484a391107552b5a61d4bc5a11
Rafy Akbar <rafyakbar02@gmail.com>
2024-01-21 22:26:28 -0500

    updated resume

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import github from "./logo-github.svg";
 import linkedin from "./logo-linkedin.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import resume from "./RafyAkbarResumeV7.pdf";
+import resume from "./RafyAkbarResumeV11.pdf";
 import photo from "./SnapseedEdit.jpg";
 
 export { github, linkedin, resume, menu, close, photo };

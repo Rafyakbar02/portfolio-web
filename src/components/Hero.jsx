37ad777ef6209a48feb3ca7ecf01fdd7d528fa61
Rafy Akbar <rafyakbar02@gmail.com>
2024-07-24 13:01:36 +0700

    finalized design for hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,13 @@ import { photo } from "../assets";
 const Hero = () => {
   return (
     <div
-      className="flex-1 py-12 flex flex-col min-[800px]:flex-row items-center justify-center gap-8"
+      className="h-full flex p-8 items-center sm:items-end"
       id="Home"
     >
-      {/* Profile Photo */}
-      <img
-        src={photo}
-        alt="photo"
-        className="m-1 h-80 w-64 object-cover rounded-lg brightness-95"
-      />
-
       {/* Header */}
-      <div className="text-center min-[800px]:text-left">
-        <h1 className="text-5xl mb-10">
-          Hello! I'm <h1 className="font-semibold">Rafy Akbar</h1>
+      <div>
+        <h1 className="text-6xl sm:text-9xl text-center sm:text-left">
+        Hello! I'm <h1 className="font-semibold">Rafy Akbar</h1>
         </h1>
       </div>
     </div>

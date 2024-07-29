ee3ec1ec181c199c721781816b19ea2a264fd84b
Rafy Akbar <rafyakbar02@gmail.com>
2024-07-29 17:20:14 +0700

    fixed build issue

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,10 +3,9 @@ import linkedin from "./logo-linkedin.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import resume from "./RafyAkbarResumeV12.pdf";
-import photo from "./SnapseedEdit.jpg";
 import catatan from "./photos/catatan.jpg";
 import stockify from "./photos/stockify.jpg";
 import degreescraper from "./photos/degreescraper.jpg";
 import nba from "./photos/nba-mvp.jpg";
 
-export { github, linkedin, resume, menu, close, photo, catatan, stockify, degreescraper, nba };
+export { github, linkedin, resume, menu, close, catatan, stockify, degreescraper, nba };

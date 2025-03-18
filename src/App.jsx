66c10a622c3374af1f579c97620f3e3a87cf6cff
Rafy Akbar <rafyakbar02@gmail.com>
2025-03-18 02:41:24 -0400

    made button only appear after window is scrolled

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,40 @@ import {
   ProjectList,
   Footer,
 } from "./components";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [showElement, setShowElement] = useState(false)
+  const scrollDistance = 950
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > scrollDistance) {
+        setShowElement(true)
+      } else {
+        setShowElement(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  })
+
   return (
     <div className="max-w-7xl m-auto">
       <div className="h-screen flex flex-col">
-        <Navbar/>
+        <Navbar />
         <Hero />
       </div>
-      <Experience /> 
+
+      <Experience />
       <ProjectList />
       <Footer />
-      <a href="#Home" className="font-merriweather bg-white fixed bottom-0 right-0 m-8 text-gray-600 border px-6 py-2 rounded-3xl my-8 w-fit">Back to Top</a>
+
+      {showElement && <a href="#Home" className="font-merriweather bg-white fixed bottom-0 right-0 m-8 text-gray-600 border px-6 py-2 rounded-3xl my-8 w-fit">Back to Top</a>}
     </div>
   );
 }

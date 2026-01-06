8b9e902386ff57a425840bc7b98ee5f01b9d26d6
Rafy Akbar <rafyakbar02@gmail.com>
2026-01-07 00:14:31 +0700

    deleted unused files

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Hero from "./Hero";
-import ProjectList from "./ProjectList";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
-import Experience from "./Experience";
-
-export { Navbar, Hero, Experience, ProjectList, Footer };
